055c18119cdbbc6df5255e5088c94a0e6194f96a
Airidas Kutra <airidas.kutra@mif.stud.vu.lt>
2021-11-05 13:33:39 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import com.alemal.gintare.validation.EmailValidator;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("BYBYS");
+		EmailValidator validator = new EmailValidator();
+		//validator.
+	}
+}
